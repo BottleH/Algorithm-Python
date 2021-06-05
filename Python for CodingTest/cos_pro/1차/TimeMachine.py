02bd6aedd4967de5439cc2de3d3c02ca4372571e
KimByeongHwan <guys1017@korea.ac.kr>
2021-06-05 14:54:45 +0900

    add.chap1

---
+++ Python for CodingTest/cos_pro/1차/TimeMachine.py
@@ -0,0 +1,12 @@
+def solution(num):
+    answer = str(num + 1)
+
+    answer = answer.replace("0", "1")
+
+    return answer
+
+
+num = 9949999;
+ret = solution(num)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
